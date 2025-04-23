bd0b9d80971b9ef66e6f457ec333796b7e361f77
John Miller <jimiller@g.syr.edu>
2025-04-23 00:07:03 +0000

    obt_sales

---
+++ northwind/models/obt_sales.sql
@@ -0,0 +1,11 @@
+with f_sales as (
+    select * from {{ ref('fact_sales') }}
+),
+d_product as (
+    select * from {{ ref('dim_product') }}
+)
+select 
+    d_product.*,
+    f.orderid, f.unitprice, f.quantity, f.discount, f.extendedprice, f.discountamount, f.soldamount
+    from f_sales as f
+    left join d_product on f.productkey = d_product.productkey
